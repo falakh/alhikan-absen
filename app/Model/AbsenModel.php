2fa2377497babacbf270b700ece72f732a5e524c
falakh <zulfikarfahmi38@gmail.com>
2020-01-26 11:19:40 +0700

    delete unused sql command

--- app/Model/AbsenModel.php
+++ app/Model/AbsenModel.php
@@ -26,7 +26,6 @@ class AbsenModel extends Model
     }
 
     public function getAllData(){
-        $hasil = DB::select('SELECT attedance.created_at,Cabang.name as cabang,users.name from attedance JOIN users join Cabang');
         return DB::table($this->getTable())
             ->select($this->getTable().".created_at",'users.name','cabang.name')
             ->join('users','users.id','=',$this->getTable().".cabangId")
