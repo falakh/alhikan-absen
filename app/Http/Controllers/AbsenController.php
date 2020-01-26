cd38abb2851506e548bcef414ce949810191f67f
falakh <zulfikarfahmi38@gmail.com>
2020-01-26 10:37:26 +0700

    fix asbesn type in databse

--- app/Http/Controllers/AbsenController.php
+++ app/Http/Controllers/AbsenController.php
@@ -8,15 +8,12 @@ use Illuminate\Http\Request;
 class AbsenController extends Controller
 {
     function PegawaiAbsen(Request $request){
-        try{
             $userId = $request->input("userId");
             $idTempat = $request->input("idTempat");
             $kehadrian = new AbsenModel();
             return $kehadrian->Absen($userId,$idTempat);
 
-        }catch (\Exception $e){
-            return $e;
-        }
+
     }
 
     function getAllData(Request $request){
