538e664f9c3091451024e4c4b85353f97a2dd54d
falakh <zulfikarfahmi38@gmail.com>
2020-02-02 22:57:41 +0700

    add laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,20 +15,8 @@ mix.react('resources/js/app.js', 'public/js')
   .sass('resources/sass/app.scss', 'public/css')
   .extract(['react', '@material-ui/core', '@material-ui/icons'])
   .setPublicPath("/")
-  
   .webpackConfig({
-    entry: {
-      index: './resources/js/app.js',
-    },
-    output: {
-      filename: '[name].js',
-      chunkFilename: '[name].js',
-      // `path` is the folder where Webpack will place your bundles
-      path: __dirname + '/',
-      // `publicPath` is where Webpack will load your bundles from (optional)
-      publicPath: __dirname + 'dist/'
-    },
-    module: {
+      module: {
       rules: [
         {
           test: /\.tsx?$/,
@@ -40,10 +28,10 @@ mix.react('resources/js/app.js', 'public/js')
     resolve: {
       alias: {
         '@material-ui/core': '@material-ui/core/esm',
-        '@material-ui/icons': '@material-ui/icons/esm'
+        '@material-ui/icons': '@material-ui/icons/esm',
       },
       extensions: ["*", ".js", ".jsx", ".ts", ".tsx"]
     }
-  });
+  }).version();
 
   
